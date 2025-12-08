593252fb9a0e0fec2f9715b4c64c8430f9b7af7b
Jesse Obinna <jesseobinna7@gmail.com>
2025-12-08 01:01:47 +0100

    updated

--- client/src/features/apiSlice.ts
+++ client/src/features/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  baseUrl: "https://api.bendonaldschools.com",
 });
 export const apiSlice = createApi({
   baseQuery,
